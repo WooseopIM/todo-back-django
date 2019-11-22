f5109804fe540a7013fc4643b6bec6bac24aea83
WooseopIM <wooseopim93@gmail.com>
2019-11-22 10:02:00 +0900

    #20191119 todo-back-django

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .serializers import TodoSerializer, UserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['POST'])
+def todo_create(request):
+    serializer = TodoSerializer(data=request.POST)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    
+    return Response(status=400)
+
+# /users/1/ => 관리자 계정
+@api_view(['GET'])
+def user_detail(request, pk):
+    # 우리의 DB를 뒤져봐(User.objects.get(pk=pk))
+    User = get_user_model()
+    user = get_object_or_404(User, pk=pk)
+    
+    # 요청이 들어온 JWT의 user 정보와 pk로 검색하여 나온 user 객체의 정보가 같을 경우
+    if request.user != user:
+        return Response(status=404)
+    serializer = UserSerializer(user)
+
+    return Response(serializer.data)
--- todoserver/urls.py
+++ todoserver/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_jwt_token), # token 발급 해주는 곳
+    path('api/v1/', include('todos.urls')),
 ]
